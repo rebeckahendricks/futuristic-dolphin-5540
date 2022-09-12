73d501bcf9c32462404dc942a12a836b2c9e0315
Rebecka Hendricks <rebecka.hendricks@gmail.com>
2022-09-12 11:50:16 -0600

    Add Amusement Park Show page and model methods

--- spec/features/amusement_parks/show_spec.rb
+++ spec/features/amusement_parks/show_spec.rb
@@ -15,7 +15,7 @@ RSpec.describe 'Amusement Park Show page' do
   describe 'As a visitor' do
     describe 'When I visit an amusement parks show page' do
       it 'I see the name and price of admissions for that amusement park' do
-        visit "amusement_parks/#{@six_flags.id}"
+        visit "/amusement_parks/#{@six_flags.id}"
 
         expect(page).to have_content(@six_flags.name)
         expect(page).to have_content(@six_flags.admission_cost)
@@ -24,17 +24,17 @@ RSpec.describe 'Amusement Park Show page' do
       end
 
       it 'I see the names of all the rides that are at that theme park listed in alphabetical order' do
-        visit "amusement_parks/#{@six_flags.id}"
+        visit "/amusement_parks/#{@six_flags.id}"
 
         expect(@ferris.name).to appear_before(@hurler.name)
         expect(@hurler.name).to appear_before(@scrambler.name)
       end
 
       it 'I see the average thrill rating of this amusement parks rides' do
-        visit "amusement_parks/#{@six_flags.id}"
+        visit "/amusement_parks/#{@six_flags.id}"
 
-        expect(page).to have_content("Average Thrill Rating of Rides: 6/10")
+        expect(page).to have_content("Average Thrill Rating of Rides: 6.0/10")
       end
     end
+    end
   end
-end
