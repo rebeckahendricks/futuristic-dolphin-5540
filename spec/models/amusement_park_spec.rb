f636def9ae7bf5579a77ba3e68b311946644cc5b
Rebecka Hendricks <rebecka.hendricks@gmail.com>
2022-09-12 11:13:11 -0600

    Create model spec for rides ordered alphabetically and average thrill rating

--- spec/models/amusement_park_spec.rb
+++ spec/models/amusement_park_spec.rb
@@ -9,4 +9,26 @@ RSpec.describe AmusementPark, type: :model do
     it { should validate_presence_of(:name) }
     it { should validate_presence_of(:admission_cost) }
   end
+
+  describe 'class methods' do
+    before :each do
+      @six_flags = AmusementPark.create!(name: 'Six Flags', admission_cost: 75)
+
+      @hurler = @six_flags.rides.create!(name: 'The Hurler', thrill_rating: 7, open: true)
+      @scrambler = @six_flags.rides.create!(name: 'The Scrambler', thrill_rating: 4, open: true)
+      @ferris = @six_flags.rides.create!(name: 'Ferris Wheel', thrill_rating: 7, open: false)
+    end
+
+    describe '.rides_ordered_alphabetically' do
+      it 'can order an amusement parks rides alphabetically' do
+        expect(AmusementPark.rides_ordered_alphabetically).to eq([@ferris, @hurler, @scrambler])
+      end
+    end
+
+    describe '.average_thrill_rating' do
+      it 'can calculate the average thrill rating of an amusement parks rides' do
+        expect(AmusementPark.average_thrill_rating).to eq(6)
+      end
+    end
+  end
 end
