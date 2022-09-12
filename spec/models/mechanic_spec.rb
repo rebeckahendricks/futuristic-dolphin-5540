3f5726486db40ca57e4ebcaefa45ae5161b92cd3
Rebecka Hendricks <rebecka.hendricks@gmail.com>
2022-09-12 09:40:48 -0600

    Create feature spec and model spec for User Story 1

--- spec/models/mechanic_spec.rb
+++ spec/models/mechanic_spec.rb
@@ -1,6 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Mechanic, type: :model do
+  before :each do
+    @mechanic1 = Mechanic.new(name: "Cooter Davenport", years_experience: 25)
+    @mechanic2 = Mechanic.new(name: "Fonzie", years_experience: 18)
+    @mechanic3 = Mechanic.new(name: "Chewbacca", years_experience: 101)
+  end
   describe 'relationships' do
     it { should have_many(:rides) }
     it { should have_many(:ride_mechanics) }
@@ -9,5 +14,11 @@ RSpec.describe Mechanic, type: :model do
   describe 'validations' do
     it { should validate_presence_of(:name) }
     it { should validate_presence_of(:years_experience) }
+  end
+
+  describe 'class methods' do
+    describe '.average_years_experience' do
+      expect(Mechanic.average_years_experience).to eq(48)
+    end
   end
 end
